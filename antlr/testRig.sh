5c536c52c94c41505b1b1cdd06784e7f553effa0
liuhelei23 <liuhelei23@mails.tsinghua.edu.cn>
2026-08-08 23:31:39 +0800

    feat: update testRig

--- antlr/testRig.sh
+++ antlr/testRig.sh
@@ -8,5 +8,6 @@ CLASS_OUTPUT_PATH="parser-classes"
 
 cd "$(dirname $0)"
 $JAVAC -cp $ANTLR_JAR parser/*.java -d $CLASS_OUTPUT_PATH
-echo "请输入要解析的字符串，输入完后请关闭输入流(Linux\Mac下按2次Ctrl+D，Windows下回车再Ctrl+Z再回车)："
+echo "Please enter the string you want to parse. After you finish entering, please close the input stream"
+echo "(press Ctrl+D twice on Linux/Mac, press Enter then Ctrl+Z then Enter on Windows):"
 $JAVA -cp $ANTLR_JAR:$CLASS_OUTPUT_PATH org.antlr.v4.gui.TestRig regex regex -gui -tokens
